adcc327eb725beedaf1fb5120d8880e2b22b84c6
beatle28c <nautilus28c@gmail.com>
2017-08-18 10:01:17 -0600

    Actualización

--- uninstall_MIPVC.sh
+++ uninstall_MIPVC.sh
@@ -4,9 +4,9 @@ rm -rf /home/SESLab
 rm -rf /usr/local/lib/python2.7/dist-packages/python_SerialKepco-1.0-py2.7.egg
 rm -rf /usr/local/lib/python2.7/dist-packages/python_HarmGen-1.0-py2.7.egg
 rm -rf /usr/local/lib/python2.7/dist-packages/python_SerialKepco-1.0-py2.7.egg
-rm -rf /usr/local/lib/python2.7/dist-packages/graphics.py
-rm -rf /usr/local/lib/python2.7/dist-packages/graphics.pyc
-rm -rf /usr/local/lib/python2.7/dist-packages/button.py
-rm -rf /usr/local/lib/python2.7/dist-packages/button.pyc
-rm -rf /home/pi/Desktop/MIPVC.desktop
-rm -rf /home/SESLab/Desktop/MIPVC.desktop
+rm /usr/local/lib/python2.7/dist-packages/graphics.py
+rm /usr/local/lib/python2.7/dist-packages/graphics.pyc
+rm /usr/local/lib/python2.7/dist-packages/button.py
+rm /usr/local/lib/python2.7/dist-packages/button.pyc
+rm /home/pi/Desktop/MIPVC.desktop
+rm /home/SESLab/Desktop/MIPVC.desktop
